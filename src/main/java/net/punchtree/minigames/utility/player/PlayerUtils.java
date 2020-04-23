12925ffe047e9b919441098b03447b2d44cbf6ad
Cxom <cxomtdoh@yahoo.com>
2020-04-23 18:50:39 -0400

    Add heart rows modifying to PlayerUtils

--- src/main/java/net/punchtree/minigames/utility/player/PlayerUtils.java
+++ src/main/java/net/punchtree/minigames/utility/player/PlayerUtils.java
@@ -1,6 +1,7 @@
 package net.punchtree.minigames.utility.player;
 
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 
 public class PlayerUtils {
@@ -11,6 +12,17 @@ public class PlayerUtils {
 		player.setSaturation(20);
 		player.setExhaustion(0);
 		player.setFireTicks(0);
+	}
+	
+	public static void giveHeartRows(Player player, int heartRows) {
+		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(heartRows * 20);
+		player.setHealth(heartRows * 20);
+	}
+	
+	public static void resetHeartRows(Player player) {
+		AttributeInstance maxHealthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+		maxHealthAttribute.setBaseValue(maxHealthAttribute.getDefaultValue());
+		player.setHealth(maxHealthAttribute.getDefaultValue());
 	}
 	
 }
