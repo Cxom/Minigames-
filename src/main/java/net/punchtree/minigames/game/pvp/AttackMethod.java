8bd3977efb7ea31d8963945088200d282900673a
Cxom <cxomtdoh@yahoo.com>
2022-11-06 10:04:56 -0500

    Add concretes to attack method counts-as-punching criteria and change unknown attack method symbol

--- src/main/java/net/punchtree/minigames/game/pvp/AttackMethod.java
+++ src/main/java/net/punchtree/minigames/game/pvp/AttackMethod.java
@@ -5,12 +5,14 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
+import java.util.Set;
+
 public enum AttackMethod{
 	
 	SLAY("🗡"),
 	SHOOT("🏹"),
 	PUNCH("ლ"),
-	UNKNOWN("?");
+	UNKNOWN("☠");
 	//ツ 
 
 	private final String icon;
@@ -22,14 +24,35 @@ public enum AttackMethod{
 	public String getIcon(){
 		return icon;
 	}
-	
+
+	private static final Set<Material> ITEMS_TO_COUNT_AS_PUNCHING = Set.of(
+			Material.AIR,
+			Material.BOW,
+			Material.WHITE_CONCRETE,
+			Material.ORANGE_CONCRETE,
+			Material.MAGENTA_CONCRETE,
+			Material.LIGHT_BLUE_CONCRETE,
+			Material.YELLOW_CONCRETE,
+			Material.LIME_CONCRETE,
+			Material.PINK_CONCRETE,
+			Material.GRAY_CONCRETE,
+			Material.LIGHT_GRAY_CONCRETE,
+			Material.CYAN_CONCRETE,
+			Material.PURPLE_CONCRETE,
+			Material.BLUE_CONCRETE,
+			Material.BROWN_CONCRETE,
+			Material.GREEN_CONCRETE,
+			Material.RED_CONCRETE,
+			Material.BLACK_CONCRETE);
+
+
 	public static AttackMethod getAttackMethod(Entity damager){
 		if (damager instanceof Arrow)
 			return AttackMethod.SHOOT;
 		else if (damager instanceof Player player)
 			if (player.getInventory().getItemInMainHand().getType() == Material.STONE_SWORD)
 				return AttackMethod.SLAY;
-			else if (player.getInventory().getItemInMainHand().getType() == Material.AIR || player.getInventory().getItemInMainHand().getType() == Material.BOW)
+			else if (ITEMS_TO_COUNT_AS_PUNCHING.contains(player.getInventory().getItemInMainHand().getType()))
 				return AttackMethod.PUNCH;
 		return AttackMethod.UNKNOWN;
 	}
