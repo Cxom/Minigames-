aaa44d61e1c1d29a9efb21853fac4323abcab95d
Jeremy Karevich <cxomtdoh@yahoo.com>
2023-09-01 17:43:10 -0400

    Add deployment tasks to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,17 @@ repositories {
     }
 }
 
+val ftpAntTask by configurations.creating
+
 dependencies {
     compileOnly("net.punchtree:punchtree-util:0.1.0-SNAPSHOT")
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+
+    ftpAntTask("org.apache.ant:ant-commons-net:1.10.12") {
+        module("commons-net:commons-net:1.4.1") {
+            dependencies("oro:oro:2.0.8:jar")
+        }
+    }
 }
 
 group = "net.punchtree"
@@ -36,4 +44,44 @@ tasks.withType<JavaCompile>() {
 
 tasks.withType<Javadoc>() {
     options.encoding = "UTF-8"
+}
+
+val ftpHostUrl: String by project
+val ftpUsername: String by project
+val ftpPassword: String by project
+val localOutputDir: String? by project
+
+task("uploadToServer") {
+    doLast{
+        ant.withGroovyBuilder {
+            "taskdef"("name" to "ftp", "classname" to "org.apache.tools.ant.taskdefs.optional.net.FTP", "classpath" to ftpAntTask.asPath)
+            "ftp"("server" to ftpHostUrl, "userid" to ftpUsername, "password" to ftpPassword, "remoteDir" to "/default/plugins") {
+                "fileset"("dir" to "build/libs") {
+                    "include"("name" to rootProject.name + "-" + version + ".jar")
+                }
+            }
+        }
+    }
+}
+
+task("buildAndPublish") {
+    dependsOn("build")
+    dependsOn("uploadToServer")
+    tasks.findByName("uploadToServer")!!.mustRunAfter("build")
+}
+
+val buildLocal by tasks.registering(Copy::class) {
+    group = "build"
+    description = "Builds the shaded JAR locally without publishing to the live server."
+
+    from("build/libs/${project.name}-${project.version}.jar")
+    into(provider {
+        if (localOutputDir != null) {
+            localOutputDir?.let { project.file(it) }
+        } else {
+            logger.warn("Environment variable LOCAL_OUTPUT_DIR is not set. Using the default output directory.")
+            project.file("build/libs")
+        }
+    })
+    dependsOn("build")
 }
